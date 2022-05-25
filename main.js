06a57ef84d65c20bcddf604ec349441ece3cb178
Jayantika2010 <79147549+jayantika51@users.noreply.github.com>
2022-05-25 16:45:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+song="";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
+function preload(){
+song=loadSound("music.mp3");
+}
+
+function setup(){
+    canvas="createCanvas(700,600)";
+    canvas.center();
+
+
+video=createCapture(VIDEO);
+video.hide();
+
+poseNet=ml5.poseNet(video, modelLoaded);
+poseNET.on('pose', gotPoses);
+}
+function modelLoaded(){
+    console.log('PoseNet Is Initialized');
+
+}
+function draw(){
+    Image(video, 0, 0, 600, 500);
+}
+
+function play(){
+    song.play();
+    song.setVolume(1);
+    song.rate(2);
+}
+
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+    leftWristX=results[0].pose.leftWrist.x;
+    leftWristY=results[0].pose.leftWrist.y;
+    console.log("leftWristX="+leftWristX+"leftWristY="+leftWristY);
+
+   rightWristX=results[0].pose.rightWrist.x;
+   rightWristY=results[0].pose.rightWrist.y;
+    console.log("rightWristX="+rightWristX+"rightWristY="+righWristY);
+}
+}
